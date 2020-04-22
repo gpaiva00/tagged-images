45a785f5ac1ab572658ea1af558b00eb612461b8
gabriel paiva <gabrielpaiva@MacBook-Pro-de-gabriel.local>
2020-04-22 13:51:04 -0300

    Moved header to another folder

--- mobile/src/components/Header/index.js
+++ mobile/src/components/Header/index.js
@@ -1,11 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { View, Text, TouchableOpacity, } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
-import Default from './Default';
-import Search from './Search';
+import styles from './styles';
 
+export default function Default() {
 
-export default function Header() {
-  return <Default />
-  // if (!showSearchHeader) return ( <Default handleSearchPress={handleSearchPress} /> );
-  // else return ( <Search handleClosePress={handleClosePress} />);
+  function handleCreatePresentation() {
+
+  }
+
+  function handleCancelPresentation() {}
+
+  return (
+    <View style={styles.header}>
+      <View style={styles.headerPageContent}>
+        <MaterialIcons
+          name="image"
+          size={28}
+          style={[styles.headerPageIcon, styles.headerIcons]}>
+          </MaterialIcons>
+
+        <Text style={styles.headerTitle}>Imagens</Text>
+      </View>
+      
+      <View style={styles.headerOptionsContent}>
+        <TouchableOpacity
+          onPress={handleCreatePresentation}
+          style={styles.headerSearchButton}
+        >
+          <MaterialIcons name='slideshow' size={28} style={styles.headerIcons}></MaterialIcons>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
